cd034223b4e47f2cd7ebac20b2bff6067c913245
Shaney Mantri <45652188+ShaneyMantri@users.noreply.github.com>
2019-09-22 15:12:10 +0530

    Second Commit

--- SAP/SAP/urls.py
+++ SAP/SAP/urls.py
@@ -22,12 +22,14 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("stock.urls")),
+    path('stock/', include("stock.urls")),
     # path('checkbox/', include("stock.urls")),
-    path('register/', users_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name='login'),
-    path('profile/', users_views.profile, name='profile'),
-    path('logout/', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name='logout'),
+    # path('register/', users_views.register, name='register'),
+    # path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name='login'),
+    # path('profile/', users_views.profile, name='profile'),
+    # path('logout/', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name='logout'),
+    path('', include("users.urls")),
+
 
 ]   
 
--- SAP/stock/urls.py
+++ SAP/stock/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name = "Stock-Home"),
-    path('', views.checkbox, name = "Stock-Checkbox"),
+    path('checkbox/', views.checkbox, name = "Stock-Checkbox"),
     path('about/', views.about, name = "Stock-About"),
 
 ]
